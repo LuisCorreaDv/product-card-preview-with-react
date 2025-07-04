df06011644596ec7ca7943c1219be6b4f80a230b
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-07-04 16:56:49 -0600

    Agregar texto al componente ProductCard y ajustar estilos de diseño

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,6 +7,7 @@ export default function ProductCard() {
             <article className="productCard_info">
                 <span className='productCard_info_header'>Perfume</span>
                 <h1 className='productCard_info_title'>Gabrielle Essence Eau De Parfum</h1>
+                <p className='productCard_info_text'>A floral, solar and voluptuous interpretation composed by <br /> Oliver Polge, Perfumer-Creater for the House of CHANEL.</p>
             </article>
         </section>
     );
