a7f6013e951380afaacabbf461b4b08b58f9ec89
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-07-04 14:44:49 -0600

    Layout del componente ProductCard junto con estilos básicos

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,13 @@
+import './styles/ProductCard.css';
 
 export default function ProductCard() {
     return (
-        <h1>Product Card</h1>
+        <section className="productCard">
+            <img src="/src/assets/images/image-product-desktop.jpg" alt="Product image" />
+            <article className="productCard_info">
+                <span>Perfume</span>
+            </article>
+        </section>
     );
 }
 
