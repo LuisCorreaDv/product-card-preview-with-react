3921fb95a87f5c8d972d598e9b1828df944f95f3
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-07-04 15:48:50 -0600

    Ajustar tamaño de card principal y posicionamiento de contenido

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,7 +5,8 @@ export default function ProductCard() {
         <section className="productCard">
             <img src="/src/assets/images/image-product-desktop.jpg" alt="Product image" />
             <article className="productCard_info">
-                <span>Perfume</span>
+                <span className='productCard_info_header'>Perfume</span>
+                <h1 className='productCard_info_title'>Gabrielle Essence Eau De Parfum</h1>
             </article>
         </section>
     );
